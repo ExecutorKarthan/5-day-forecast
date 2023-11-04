e4e083db30ef8e331e5fd5605cef5343081f5904
Alex Messina <me@alexmessina.dev>
2023-11-03 23:13:57 -0500

    Data is displayed in app -Added HTML containers for Data -Added JS to retrieve weather data and populate containers -Moved away from localstorage as it was not effective

--- script.js
+++ script.js
@@ -14,29 +14,47 @@ function getCity(){
         fetch(geoURL).then(function (response) {
           return response.json();
       }).then(function(data){
-        var coord = String(data[0].lat) + "," + String(data[0].lon) + "," + data[0].country + "," + data[0].state +","
-        localStorage.setItem(data[0].name, coord)
-        getWeatherData(data[0].name);
+        var coord = String(data[0].lat) + "," + String(data[0].lon);
+        getWeatherData(data[0].name, coord);
       })
     }
 }
 
-function getWeatherData(city) {
-  var cityData = localStorage.getItem(city).split(",")
-  var lat = cityData[0]
-  var lon = cityData[1]
-
+function getWeatherData(city, coord) {
+  var cityName = city
+  var coord = coord.split(",")
+  var lat = coord[0]
+  var lon = coord[1]
   var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
   fetch(currentWeatherUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data)
       var now = new Date
       var date = String(now.getMonth()+1) + "/" + String(now.getDate()) + "/" + String(now.getFullYear())
-      var tempInF = (data.main.temp-273.15)*(9/5)+32
-      var currentData = data.name + "," + date + "," + String(Math.round(tempInF)) + "," +  String(Math.round(data.wind.speed))  + "mph," + String(Math.round(data.main.humidity))+"%,";
-      localStorage.setItem("currentDay", currentData)
+      
+      var cityTitle = document.querySelector("#cityHeader")
+      cityTitle.innerHTML = cityName + "(" + date + ")";
+
+      var weatherIconURL = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+      var currentIcon = document.querySelector("#currentIcon")
+      currentIcon.src = weatherIconURL;
+      
+      
+      var tempEle = document.querySelector("#currentTemp")
+      var tempInF = Math.round(((data.main.temp-273.15)*(9/5)+32)*100)/100
+      tempEle.innerHTML = "Temp: " + tempInF + "°F"
+      
+      var windEle = document.querySelector("#currentWind")
+      var wind = "Wind: " + String(Math.round(data.wind.speed))+ "mph"
+      windEle.innerHTML = wind
+      
+      var humidEle = document.querySelector("#currentHumid")
+      var humid = "Humidity: " + String(Math.round(data.main.humidity))+"%";
+      humidEle.innerHTML = humid
+      
     });
 
     var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
@@ -45,18 +63,33 @@ function getWeatherData(city) {
         return response.json();
       })
       .then(function (data) {
-        var forecast = "";
+        console.log(data)
+        var now = new Date;
+        var count = 1;
         for(var i = 0; i< 40; i = i+8){
-          var tempInF = (data.list[i].main.temp-273.15)*(9/5)+32
-          forecast = forecast + data.list[i].dt_txt.substring(0, 10) + "," + data.list[i].weather[0].icon + "," + String(Math.round(tempInF)) + "," + String(Math.round(data.list[i].wind.speed)) + "mph," + String(Math.round(data.list[i].main.humidity))+"%,";
+          setForecast(now, count, i, data);
+          count++;
         }
-        var forecastUpdate = localStorage.getItem(city) + forecast
-        localStorage.setItem(city, forecastUpdate)
       });
-  updateDisplay();
 }
 
-function updateDisplay(){
-  var dataArray = localStorage.getItem(cityName).split(",")
-  console.log(dataArray)
+function setForecast(now, count, i, data){
+  var date = String(now.getMonth()+1) + "/" + String(now.getDate()+count) + "/" + String(now.getFullYear())  
+  var dateHeader = document.querySelector("#date"+String(count))
+  
+  var weatherIconURL = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+  var icon = document.querySelector("#icon"+String(count))
+  icon.src = weatherIconURL;
+
+  var tempEle = document.querySelector("#temp"+String(count))
+  var tempInF = Math.round(((data.list[i].main.temp-273.15)*(9/5)+32)*100)/100
+  tempEle.innerHTML = "Temp: " + tempInF + "°F"
+      
+  var windEle = document.querySelector("#wind"+String(count))
+  var wind = "Wind: " + String(Math.round(data.list[i].wind.speed))+ "mph"
+  windEle.innerHTML = wind
+      
+  var humidEle = document.querySelector("#humid"+String(count))
+  var humid = "Humidity: " + String(Math.round(data.list[i].main.humidity))+"%";
+  humidEle.innerHTML = humid  
 }
